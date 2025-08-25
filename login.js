61dfd84ab7768f1df3a76bc39d1a19dbde23df20
John Doe <appcresi@gmail.com>
2025-08-25 10:58:17 -0300

    panel de control

---
+++ login.js
@@ -0,0 +1,23 @@
+const ver = document.getElementById("verpassword")
+ver.addEventListener("change", vercontrasena )
+
+function vercontrasena() {
+    if (ver.checked === true) {
+        document.getElementById("inppassword").type= "text"
+        document.getElementById("inpconfirmar").type= "text"
+    }else{
+        document.getElementById("inppassword").type= "password"
+        document.getElementById("inpconfirmar").type= "password"
+    }
+}
+const botoningresar = document.getElementById("btniniciar")
+botoningresar.addEventListener("click", ingresar )
+function ingresar() {
+    alert("Está por ingresar")
+}
+const botonregistrar = document.getElementById("btnregistrarse")
+botonregistrar.addEventListener("click", verregistrar )
+function verregistrar() {
+    document.getElementById("confirmar").style.display = "block"
+    document.getElementById("inpconfirmar").style.display = "block"
+}
